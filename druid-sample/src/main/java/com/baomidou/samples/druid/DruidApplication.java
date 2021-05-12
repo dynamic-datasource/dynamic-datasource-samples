c0de9a7a3ebd1cefff8362fd10272d9fd755bfed
小锅盖 <332309254@qq.com>
2021-05-12 23:01:59 +0800

    3.3.3

--- druid-sample/src/main/java/com/baomidou/samples/druid/DruidApplication.java
+++ druid-sample/src/main/java/com/baomidou/samples/druid/DruidApplication.java
@@ -1,13 +1,12 @@
 package com.baomidou.samples.druid;
 
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
-@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)// ←←←←← look at here
+@SpringBootApplication
 @MapperScan("com.baomidou.samples.druid.mapper")
 public class DruidApplication {
 
