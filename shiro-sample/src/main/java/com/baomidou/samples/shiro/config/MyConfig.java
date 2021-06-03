21a67e9a92dc7e6d1de8eeb17d3aaa2d5c57448b
小锅盖 <332309254@qq.com>
2021-06-03 10:05:44 +0800

    fix shiro

--- shiro-sample/src/main/java/com/baomidou/samples/shiro/config/MyConfig.java
+++ shiro-sample/src/main/java/com/baomidou/samples/shiro/config/MyConfig.java
@@ -1,6 +1,21 @@
+/*
+ * Copyright © ${project.inceptionYear} organization baomidou
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.baomidou.samples.shiro.config;
 
-import com.baomidou.dynamic.datasource.aop.DynamicDatasourceNamedInterceptor;
+//import com.baomidou.dynamic.datasource.aop.DynamicDatasourceNamedInterceptor;
 import com.baomidou.dynamic.datasource.processor.DsProcessor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.apache.shiro.SecurityUtils;
@@ -31,11 +46,11 @@ public class MyConfig {
 //        attributes.setProperty("add*", "master");
 //        attributes.setProperty("update*", "master");
 //        attributes.setProperty("delete*", "master");
-        DynamicDatasourceNamedInterceptor advice = new DynamicDatasourceNamedInterceptor(dsProcessor);
-        advice.fromProperties(attributes);
+//        DynamicDatasourceNamedInterceptor advice = new DynamicDatasourceNamedInterceptor(dsProcessor);
+//        advice.fromProperties(attributes);
 
         advisor.setPointcut(pointcut);
-        advisor.setAdvice(advice);
+//        advisor.setAdvice(advice);
         return advisor;
     }
 
