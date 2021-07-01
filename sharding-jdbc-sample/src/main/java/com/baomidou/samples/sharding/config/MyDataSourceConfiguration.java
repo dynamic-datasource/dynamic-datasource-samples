7539a1d4dd85490ee604e7af0a75e6e5de9163b8
DESKTOP-M755TSN\TaoYu <332309254@qq.com>
2021-07-01 16:20:29 +0800

    sharding

--- sharding-jdbc-sample/src/main/java/com/baomidou/samples/sharding/config/MyDataSourceConfiguration.java
+++ sharding-jdbc-sample/src/main/java/com/baomidou/samples/sharding/config/MyDataSourceConfiguration.java
@@ -18,20 +18,17 @@ package com.baomidou.samples.sharding.config;
 import com.baomidou.dynamic.datasource.DynamicRoutingDataSource;
 import com.baomidou.dynamic.datasource.provider.AbstractDataSourceProvider;
 import com.baomidou.dynamic.datasource.provider.DynamicDataSourceProvider;
-import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.DataSourceProperty;
-import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.DynamicDataSourceAutoConfiguration;
 import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.DynamicDataSourceProperties;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
+import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-@AutoConfigureBefore(DynamicDataSourceAutoConfiguration.class)
 public class MyDataSourceConfiguration {
 
     @Resource
@@ -47,14 +44,13 @@ public class MyDataSourceConfiguration {
 
     @Bean
     public DynamicDataSourceProvider dynamicDataSourceProvider() {
-        Map<String, DataSourceProperty> datasourceMap = properties.getDatasource();
         return new AbstractDataSourceProvider() {
 
             @Override
             public Map<String, DataSource> loadDataSources() {
-                Map<String, DataSource> dataSourceMap = createDataSourceMap(datasourceMap);
+                Map<String, DataSource> dataSourceMap = new HashMap<>();
                 dataSourceMap.put("sharding", masterSlaveDataSource);
-                //打开下面的代码可以把 shardingjdbc 管理的数据源也交给动态数据源管理 (根据自己需要选择开启)
+                //打开下面的代码可以把 shardingJdbc 内部管理的子数据源也同时添加到动态数据源里 (根据自己需要选择开启)
 //                dataSourceMap.putAll(((MasterSlaveDataSource) masterSlaveDataSource).getDataSourceMap());
                 return dataSourceMap;
             }
@@ -64,7 +60,7 @@ public class MyDataSourceConfiguration {
     /**
      * 将动态数据源设置为首选的
      * 当spring存在多个数据源时, 自动注入的是首选的对象
-     * 设置为主要的数据源之后，就可以支持shardingjdbc原生的配置方式了
+     * 设置为主要的数据源之后，就可以支持shardingJdbc原生的配置方式了
      */
     @Primary
     @Bean
