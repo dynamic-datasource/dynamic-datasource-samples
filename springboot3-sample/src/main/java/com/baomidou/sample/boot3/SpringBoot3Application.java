462e6edd9f275b0f6bf7b40edad90e5952fa73f8
TaoYu <332309254@qq.com>
2023-06-27 20:23:22 +0800

    up

---
+++ springboot3-sample/src/main/java/com/baomidou/sample/boot3/SpringBoot3Application.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright © ${project.inceptionYear} organization baomidou
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.baomidou.sample.boot3;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+@MapperScan("com.baomidou.sample.boot3.mapper")
+public class SpringBoot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3Application.class, args);
+        log.info("open http://localhost:8080/swagger-ui.html");
+    }
+}
